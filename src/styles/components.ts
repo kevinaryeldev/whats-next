7b872ed69c686d099671c79160ee526486befd55
Kevin <kevinaryel.dev@gmail.com>
2022-07-05 09:04:10 -0300

    feat(Theme): custom components styled

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -4,9 +4,28 @@ const components: Record<string, StyleConfig> = {
   PageWrapper: {
     baseStyle: ({ colorMode }) => ({
       bg: colorMode === 'dark' ? 'gray.100' : 'gray.400',
-      W: '100%',
-      H: '100vh',
-      minH: 'fit-content',
+      width: '100%',
+      height: 'fit-content',
+      minH: '100vh',
+    }),
+  },
+  ButtonNew: {
+    baseStyle: ({ colorMode }) => ({
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 'xl',
+      width: 'fit-content',
+      gap: '0.25rem',
+      padding: ['0.25rem', '0.5rem'],
+      bg: colorMode === 'dark' ? 'blueD.100' : 'blueL.100',
+      color: colorMode == 'dark' ? 'gray.100' : 'gray.400',
+      _hover: {
+        bg: colorMode === 'dark' ? 'blueD.200' : 'blueL.200',
+      },
+      _active: {
+        bg: colorMode === 'dark' ? 'blueD.300' : 'blueL.300',
+      },
     }),
   },
 }
