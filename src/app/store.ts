05aaba2b226f755376caa0ca2f21d72a174bb2f4
Kevin <kevinaryel.dev@gmail.com>
2022-07-12 21:32:03 -0300

    feat(redux): Added taskReducer to Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './features/admin/adminSlice'
+import tasksReducer from './features/tasks/tasksSlice'
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: { user: userReducer, tasks: tasksReducer },
 })
 
 export type RootState = ReturnType<typeof store.getState>
