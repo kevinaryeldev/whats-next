e21fe64ea4e022387bc9f312c0ba285d3a0ac645
Kevin <kevinaryel.dev@gmail.com>
2022-07-13 23:10:00 -0300

    fix(fetchData):working for current userId

--- src/services/data.services.ts
+++ src/services/data.services.ts
@@ -1,7 +1,8 @@
 import { api } from './api'
+import { getId } from './auth.header'
 
 const fetchData = () => {
-  return api.get('/tasks?userId=1').then((response) => {
+  return api.get(`/tasks?userId=${getId()}`).then((response) => {
     return response.data
   })
 }
